58a596c92d3a0c668027f48944ff43c390f22ffa
trs998 <trs998@gmail.com>
2019-01-02 19:30:44 +0000

    Added dropdown support

--- YVTS-CourseManager/functions/yvts_coursemanager_application.php
+++ YVTS-CourseManager/functions/yvts_coursemanager_application.php
@@ -103,6 +103,14 @@ function yvts_coursemanager_application($attributes) {
                         $output = $output . "
                         error = error + \"\\n\\n\";
                     }\n";
+                } else if (($fields[$i]->minlength > 0) && ($fields[$i]->type == "dropdown")) {
+                    $output = $output . "if (document.getElementById(\"yvts_field_" . $fields[$i]->applicationid . "\").value.length < " . $fields[$i]->minlength . ") {
+					    error = error + \"" . $fields[$i]->name . ": \";
+                        ";
+                        $output = $output . "error = error + \"Must have an option selected\";";
+                        $output = $output . "
+                        error = error + \"\\n\\n\";
+                    }\n";
                 }
 			}
 			
@@ -201,6 +209,18 @@ function yvts_coursemanager_application($attributes) {
                     $text = $text . stripcslashes($fields[$i]->name) . " : " . $examsticked . "\r\n<br />";
                     $csvoutputtop = $csvoutputtop . "\"" . $fields[$i]->name . "\",";
                     $csvoutputdata = $csvoutputdata . "\"" . $examsticked . "\",";
+                } else if ($fields[$i]->type == "dropdown") {
+					if ((strlen($_POST["yvts_field_" . $fields[$i]->applicationid]) < 1) && ($fields[$i]->minlength >= 1)) {
+						$text = $text . stripcslashes($fields[$i]->name) . " : " . $_POST["yvts_field_" . $fields[$i]->applicationid] . "\r\n<br />";
+						$csvoutputtop = $csvoutputtop . "\"" . $fields[$i]->name . "\",";
+						$csvoutputdata = $csvoutputdata . "\"" . $_POST["yvts_field_" . $fields[$i]->applicationid] . "\",";
+                        $errors = $errors . stripcslashes($fields[$i]->name) . ": " . stripcslashes($fields[$i]->hint) . "\n";
+					} else {
+					//not required, add whatever has arrived
+						$text = $text . stripcslashes($fields[$i]->name) . " : " . $_POST["yvts_field_" . $fields[$i]->applicationid] . "\r\n<br />";
+						$csvoutputtop = $csvoutputtop . "\"" . $fields[$i]->name . "\",";
+						$csvoutputdata = $csvoutputdata . "\"" . $_POST["yvts_field_" . $fields[$i]->applicationid] . "\",";
+					}
                 } else if ($fields[$i]->type == "captcha") {
                     $captchapublic = get_option("yvts_coursemanager_captcha_public");
                     $captchaprivate = get_option("yvts_coursemanager_captcha_private");
@@ -322,6 +342,22 @@ function yvts_coursemanager_application($attributes) {
                 if (strlen($_POST["yvts_field_" . $fields[$i]->applicationid . ""]) >= $fields[$i]->minlength) { $output = $output . " style=\"background-color: #d1ffd2\" "; } else { $output = $output . " style=\"background-color: pink\" "; }
                 }
                 $output = $output . " oninput=\"yvts_validate_box('yvts_field_" . $fields[$i]->applicationid . "'," . $fields[$i]->minlength . ");\" />";
+                } elseif ($fields[$i]->type == "dropdown") { //dropdown list
+					//dropdown values
+					$values = explode("|",$fields[$i]->hint);
+                    $output = $output . "<select id=\"yvts_field_" . $fields[$i]->applicationid . "\" name=\"yvts_field_" . $fields[$i]->applicationid . "\" ";
+					if ($fields[$i]->minlength > 0) { //set hint colours
+					if (strlen($_POST["yvts_field_" . $fields[$i]->applicationid . ""]) >= $fields[$i]->minlength) { $output = $output . " style=\"background-color: #d1ffd2\" "; } else { $output = $output . " style=\"background-color: pink\" "; }
+					}
+					$output = $output . " oninput=\"yvts_validate_box('yvts_field_" . $fields[$i]->applicationid . "'," . $fields[$i]->minlength . ");\">";
+					foreach ($values as $value) {
+						$output = $output . "\t<option";
+						if ((isset($_POST["yvts_field_" . $fields[$i]->applicationid . ""])) && ($_POST["yvts_field_" . $fields[$i]->applicationid . ""] == $value)) {
+							$output = $output . " selected=\"selected\" ";
+						}
+						$output = $output . ">" . $value . "</option>\r\n";
+					}
+					$output = $output . "</select>\r\n";
                 } elseif ($fields[$i]->type == "textarea") {
                     $output = $output . "<div class=\"yvts_field_textarea\">"  . stripcslashes($fields[$i]->hint) . "</div> <textarea id=\"yvts_field_" . $fields[$i]->applicationid . "\" name=\"yvts_field_" . $fields[$i]->applicationid . "\" ";
                     if ($fields[$i]->minlength > 0) { //set hint colours
